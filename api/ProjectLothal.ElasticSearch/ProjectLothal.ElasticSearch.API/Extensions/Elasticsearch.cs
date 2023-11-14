3464b1679a265468480925905a8b521ff80b312c
utkudemr <utkuudemr@gmail.com>
2023-11-14 22:55:37 +0300

    index mapping blog class

--- api/ProjectLothal.ElasticSearch/ProjectLothal.ElasticSearch.API/Extensions/Elasticsearch.cs
+++ api/ProjectLothal.ElasticSearch/ProjectLothal.ElasticSearch.API/Extensions/Elasticsearch.cs
@@ -1,6 +1,7 @@
 ﻿using Elasticsearch.Net;
 using MassTransit;
 using Nest;
+using ProjectLothal.ElasticSearch.Domain.Models;
 using System.Reflection;
 
 namespace ProjectLothal.ElasticSearch.API.Extensions
@@ -11,6 +12,17 @@ namespace ProjectLothal.ElasticSearch.API.Extensions
             var elasticPool = new SingleNodeConnectionPool(new Uri(configuration.GetSection("Elastic")["Url"]!));
             var settings = new ConnectionSettings(elasticPool);
             var client = new ElasticClient(settings);
+           client.Indices
+                .Create("blog", a => a.Map<Blog>(m => m
+                    .Properties(ps => ps
+                       .Text(s => s.Name(a=>a.Title).Fields(a => a.Keyword(s => s.Name(a=>a.Title))))
+                       .Keyword(q=>q.Name(a=>a.UserId))
+                       .Keyword(q=>q.Name(a=>a.Content))
+                       .Keyword(q=>q.Name(a=>a.Created))
+                       .Date(q=>q.Name(a=>a.Created))
+                       )
+
+                    ));
             services.AddSingleton(client);
         }
 
---
+++ api/ProjectLothal.ElasticSearch/ProjectLothal.ElasticSearch.Domain/Models/Blog.cs
@@ -0,0 +1,19 @@
+﻿
+
+using Nest;
+
+namespace ProjectLothal.ElasticSearch.Domain.Models;
+
+public class Blog
+{
+    [Text(Name = "title")]
+    public string Title { get; set; }
+    [Text(Name = "content")]
+    public string Content { get; set; }
+    [Text(Name = "user_id")]
+    public string UserId { get; set; }
+    [Text(Name = "tags")]
+    public string[] Tags { get; set; }
+    [Text(Name = "created")]
+    public DateTime Created { get; set; }
+}
